8ad03769160549c7557639d815736e5a955bfd4f
klimov-rv <klimov.rv@yandex.ru>
2026-02-16 12:23:04 +0300

    fix: bring upload fileld to backend-naming "files"

--- frontend-placeholder/src/services/fetch/auth/storage/SendUploadFIle.js
+++ frontend-placeholder/src/services/fetch/auth/storage/SendUploadFIle.js
@@ -1,15 +1,11 @@
 import axios from "axios";
-import {API_FILES} from "../../../../UrlConstants.jsx";
-import StorageExceedException from "../../../../exception/StorageExceedException.jsx";
+import { API_FILES } from "../../../../UrlConstants.jsx";
 import bytes from "bytes";
-
 
 export async function sendUpload(files, updateDownloadTask, updateTask, uploadTask, currPath) {
     if (import.meta.env.VITE_MOCK_FETCH_CALLS) {
         console.log("Mocked fetch call for upload file");
-
         updateTask(uploadTask, "completed", "Загружено");
-
         return;
     }
 
@@ -17,14 +13,13 @@ export async function sendUpload(files, updateDownloadTask, updateTask, uploadTa
     console.log(files);
 
     const formData = new FormData();
-    files.forEach(({file, path}) => {
-        formData.append("object", file, path);
-    })
+    files.forEach(({ file, path }) => {
+        formData.append("files", file, path);
+    });
     formData.append("path", currPath);
 
-
     try {
-        console.log("Отправляем файлы на бэк ");
+        console.log("Отправляем файлы на бэк");
 
         const response = await axios.post(API_FILES, formData, {
             headers: {
@@ -34,9 +29,8 @@ export async function sendUpload(files, updateDownloadTask, updateTask, uploadTa
             onUploadProgress: (progressEvent) => {
                 updateTask(uploadTask, "progress", "Загружаем... " + bytes(progressEvent.rate) + "/c");
                 if (progressEvent.progress === 1) {
-                    updateTask(uploadTask, "progress", "Сохраняем в хранилище...")
+                    updateTask(uploadTask, "progress", "Сохраняем в хранилище...");
                 }
-
                 updateDownloadTask(uploadTask, progressEvent.progress * 100);
             },
         });
@@ -47,13 +41,10 @@ export async function sendUpload(files, updateDownloadTask, updateTask, uploadTa
     } catch (error) {
         console.log(error);
 
-        if (error.response.status === 409) {
+        if (error.response?.status === 409) {
             updateTask(uploadTask, "error", "Файл/папка с таким именем уже существует в целевой папке!");
-
         } else {
             updateTask(uploadTask, "error", "Ошибка при загрузке. Попробуйте позже");
         }
     }
-
-
 }
